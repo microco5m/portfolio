0c77e0d538c42332c62043cfb8bbf306f38db739
microco5m <1uvvw11@gmail.com>
2025-03-19 17:49:35 +0900

    0319-1749

--- js/index.js
+++ js/index.js
@@ -1,39 +1,45 @@
-//tab-menu
+window.onload = function() {
+  
+  //tab-menu
+  
+  const menuBtn = document.querySelector("#mobile-btn");
+  const mainMenu = document.querySelector(".main-menu");
 
-const menuBtn = document.querySelector("#mobile-btn");
+  menuBtn.addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('active');
+    
+  })
+  
+  //Intro
+  
+  
+  
+  
+  
+  
+  
+  //skill
+  let progressCircles = document.querySelectorAll(".progress-circle");
+  
+  progressCircles.forEach((circle) => {
+    let progressNum = circle.querySelector(".progress-num");
+    progressNum.textContent = "0%"
+  
+    let progressEndNum = parseInt(circle.getAttribute("data-percent"));
+    let progressStartNum = 0;
+  
+    let progress = setInterval(() => {
+      progressStartNum ++;
+      progressNum.textContent = `${progressStartNum}%`;
+  
+      circle.style.background = `conic-gradient(#3E81FF ${progressStartNum * 3.6}deg, #fff 0deg)`
+  
+      if (progressStartNum == progressEndNum) {
+        clearInterval(progress);
+      }
+    },20)
+  })
+  
 
-menuBtn.addEventListener('click', (e) => {
-  e.currentTarget.classList.toggle('active');
-})
 
-//Intro
-
-
-
-
-
-
-
-//skill
-let progressCircles = document.querySelectorAll(".progress-circle");
-
-progressCircles.forEach((circle) => {
-  let progressNum = circle.querySelector(".progress-num");
-  progressNum.textContent = "0%"
-
-  let progressEndNum = parseInt(circle.getAttribute("data-percent"));
-  let progressStartNum = 0;
-
-  let progress = setInterval(() => {
-    progressStartNum ++;
-    progressNum.textContent = `${progressStartNum}%`;
-
-    circle.style.background = `conic-gradient(#3E81FF ${progressStartNum * 3.6}deg, #fff 0deg)`
-
-    if (progressStartNum == progressEndNum) {
-      clearInterval(progress);
-    }
-  },20)
-})
-
-
+}  
